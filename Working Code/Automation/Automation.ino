e31bb5f5b36def7ff15b64164e4c09cb93200a4a
WillWashburn <w.sharp.washburn@gmail.com>
2019-09-17 12:13:38 -0400

    Time

--- Working Code/Automation/Automation.ino
+++ Working Code/Automation/Automation.ino
@@ -2,9 +2,9 @@
 
 //Time start Settings:
 
-int startingHour = 15; // set your starting hour here, not below at int hour. This ensures accurate daily correction of time
+int startingHour = 16; // set your starting hour here, not below at int hour. This ensures accurate daily correction of time
 int seconds = 0;
-int minutes = 3;
+int minutes = 11;
 int hours = startingHour;
 int days = 0;
 
