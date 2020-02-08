363cc2e820083a434c6be06f14017a6dc08399c0
Michael Xu <anaxciot@gmail.com>
2020-02-08 15:23:57 -0500

    Fixed syntax errors and included math.h

--- Working_Code/CableSpeedController/Applying the Maths/StepCount/StepCount.ino
+++ Working_Code/CableSpeedController/Applying the Maths/StepCount/StepCount.ino
@@ -1,3 +1,5 @@
+#include <math.h>
+
 //button pins
 int buttonRight = A2;
 int buttonLeft = A3;
@@ -34,15 +36,16 @@ bool buttonUpState;
 bool buttonDownState;
 
 //Global Variables
-int MC = 32;        //Microstep setting
+int mc = 32;        //Microstep setting
 int dia = 2;        //Gear diameter in inches
-int base = 500;     //Base workspace dimension in inches
-int height = 500;   //Height workspace dimension in inches
+int B = 70;     //Base workspace dimension in inches
+int H = 60;   //Height workspace dimension in inches
 
 double x = 0;             //Current x-position
 double y = 0;             //Current y-position
 double stepCountHor = 0;  //Horizontal step counter
 double stepCountVer = 0;  //Vertical step counter
+double nbrOfSteps =0;    //Step Counter
 
 void setup() {
   // put your setup code here, to run once:
@@ -120,124 +123,125 @@ void loop() {
       moveCartDown()
     }
   */
-  void moveCartLeft() {
-    setDir(DIR_left, HIGH); setDir(DIR_right, LOW);                             //set the direction of the primary motors to whatever would move the cart the correct direction
-    stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);          //step the primary cables once
-    nbrOfSteps = calcSecondaryStep(x, y, 3);                                    //calculate the cooresponding nbr of steps the secondary need to take, the direction the cart is moving with impact if the number is negative of positive
-    stepCountVer = stepCountVer + nbrOfSteps;                                   //add the nbr of steps to the step count, this should be a global variable
-
-    //stepCountVer will be positive if needs to move slightly out (loosen) and negative if needs to move slightly in (tightened)
-
-    if (stepCountVer > 1) {
-      setDir(DIR_up, HIGH); setDir(DIR_down, HIGH); //set direction that would tighten the cables
-      stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay); //step both motors
-      stepCountVer = stepCountVer - 1;
-    }
-      if (stepCountVer < -1) {
-        setDir(DIR_up, LOW); setDir(DIR_down, LOW);
-        stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
-        stepCountVer = stepCountVer + 1;
-      }
-    }
-
-  void moveCartRight() {
-    setDir(DIR_left, LOW); setDir(DIR_right, HIGH);
-    stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
-    nbrOfSteps = calcSecondaryStep(x, y, 4);
-    stepCountVer = stepCountVer + nbrOfSteps;
-
-    if (stepCountVer > 1) {
-      setDir(DIR_up, HIGH); setDir(DIR_down, HIGH);
-      stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
-      stepCountVer = stepCountVer - 1;
-    }
-
-    if (stepCountVer < -1) {
-      setDir(DIR_up, LOW); setDir(DIR_down, LOW);
-      stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
-      stepCountVer = stepCountVer + 1;
-    }
-  }
-
-  void moveCartUp() {
-    setDir(DIR_up, HIGH); setDir(DIR_down, LOW);
-    stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
-    nbrOfSteps = calcSecondaryStep(x, y, 1);
-    stepCountHor = stepCountHor + nbrOfSteps;
-
-    if (stepCountHor > 1) {
-      setDir(DIR_left, HIGH); setDir(DIR_right, HIGH);
-      stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
-      stepCountHor = stepCountHor - 1;
-    }
-
-    if (stepCountHor < -1) {
-      setDir(DIR_left, LOW); setDir(DIR_right, LOW);
-      stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
-      stepCountHor = stepCountHor + 1;
-    }
-  }
-
-  void moveCartDown() {
-    setDir(DIR_up, LOW); setDir(DIR_down, HIGH);
-    stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
-    nbrOfSteps = calcSecondaryStep(x, y, 2);
-    stepCountHor = stepCountHor + nbrOfSteps;
-
-    if (stepCountHor > 1) {
-      setDir(DIR_left, HIGH); setDir(DIR_right, HIGH);
-      stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
-      stepCountHor = stepCountHor - 1;
-    }
-
-    if (stepCountHor < -1) {
-      setDir(DIR_left, LOW); setDir(DIR_right, LOW);
-      stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
-      stepCountHor = stepCountHor + 1;
-    }
-  }
-
-  void setDir(int motor, bool dir) {
-    digitalWrite(motor, dir);
-  }
-
   int delaytime = setDelay();
   buttonLeftState = digitalRead(buttonLeft);
   buttonRightState = digitalRead(buttonRight);
   buttonUpState = digitalRead(buttonUp);
   buttonDownState = digitalRead(buttonDown);
 
-  if (buttonLeftState == LOW) {
+  /*if (buttonLeftState == LOW) {
     //Serial.println("left button pressed");
 
     digitalWrite(PUL_left, LOW);
     delayMicroseconds(delaytime);
     digitalWrite(PUL_left, HIGH);
     delayMicroseconds(delaytime);
-  }
+    }
 
-  if (buttonRightState == LOW) {
+    if (buttonRightState == LOW) {
 
     digitalWrite(PUL_right, LOW);
     delayMicroseconds(delaytime);
     digitalWrite(PUL_right, HIGH);
     delayMicroseconds(delaytime);
-  }
+    }
 
-  if (buttonUpState == LOW) {
+    if (buttonUpState == LOW) {
     //Serial.println("up button pressed");
 
     digitalWrite(PUL_up, LOW);
     delayMicroseconds(delaytime);
     digitalWrite(PUL_up, HIGH);
     delayMicroseconds(delaytime);
-  }
+    }
 
-  if (buttonDownState == LOW) {
+    if (buttonDownState == LOW) {
     //Serial.println("down button pressed");
 
     stepMotor(PUL_down, delaytime);
+    }
+  */
+}
+
+void moveCartLeft() {
+  setDir(DIR_left, HIGH); setDir(DIR_right, LOW);                             //set the direction of the primary motors to whatever would move the cart the correct direction
+  stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);          //step the primary cables once
+  nbrOfSteps = calcSecondaryStep(x, y, 3);                                    //calculate the cooresponding nbr of steps the secondary need to take, the direction the cart is moving with impact if the number is negative of positive
+  stepCountVer = stepCountVer + nbrOfSteps;                                   //add the nbr of steps to the step count, this should be a global variable
+
+  //stepCountVer will be positive if needs to move slightly out (loosen) and negative if needs to move slightly in (tightened)
+
+  if (stepCountVer > 1) {
+    setDir(DIR_up, HIGH); setDir(DIR_down, HIGH); //set direction that would tighten the cables
+    stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay); //step both motors
+    stepCountVer = stepCountVer - 1;
   }
+  if (stepCountVer < -1) {
+    setDir(DIR_up, LOW); setDir(DIR_down, LOW);
+    stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
+    stepCountVer = stepCountVer + 1;
+  }
+}
+
+void moveCartRight() {
+  setDir(DIR_left, LOW); setDir(DIR_right, HIGH);
+  stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
+  nbrOfSteps = calcSecondaryStep(x, y, 4);
+  stepCountVer = stepCountVer + nbrOfSteps;
+
+  if (stepCountVer > 1) {
+    setDir(DIR_up, HIGH); setDir(DIR_down, HIGH);
+    stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
+    stepCountVer = stepCountVer - 1;
+  }
+
+  if (stepCountVer < -1) {
+    setDir(DIR_up, LOW); setDir(DIR_down, LOW);
+    stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
+    stepCountVer = stepCountVer + 1;
+  }
+}
+
+void moveCartUp() {
+  setDir(DIR_up, HIGH); setDir(DIR_down, LOW);
+  stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
+  nbrOfSteps = calcSecondaryStep(x, y, 1);
+  stepCountHor = stepCountHor + nbrOfSteps;
+
+  if (stepCountHor > 1) {
+    setDir(DIR_left, HIGH); setDir(DIR_right, HIGH);
+    stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
+    stepCountHor = stepCountHor - 1;
+  }
+
+  if (stepCountHor < -1) {
+    setDir(DIR_left, LOW); setDir(DIR_right, LOW);
+    stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
+    stepCountHor = stepCountHor + 1;
+  }
+}
+
+void moveCartDown() {
+  setDir(DIR_up, LOW); setDir(DIR_down, HIGH);
+  stepMotor(PUL_up, motordelay); stepMotor(PUL_down, motordelay);
+  nbrOfSteps = calcSecondaryStep(x, y, 2);
+  stepCountHor = stepCountHor + nbrOfSteps;
+
+  if (stepCountHor > 1) {
+    setDir(DIR_left, HIGH); setDir(DIR_right, HIGH);
+    stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
+    stepCountHor = stepCountHor - 1;
+  }
+
+  if (stepCountHor < -1) {
+    setDir(DIR_left, LOW); setDir(DIR_right, LOW);
+    stepMotor(PUL_left, motordelay); stepMotor(PUL_right, motordelay);
+    stepCountHor = stepCountHor + 1;
+  }
+}
+
+void setDir(int motor, bool dir) {
+  digitalWrite(motor, dir);
 }
 
 //I think since the motion is based on step count, the delayTime will stay constant, so this argument could be removed
@@ -279,15 +283,15 @@ int setDelay() {
 //delta returns the change of length in the top cable over the change in length of the horizontal cable
 double delta(double x, double y) {
   double dx = (dia * PI) / (200 * mc); double dy = dx;
-  double ACD = sqrt(x ^ 2 + (H - y) ^ 2) + sqrt((B - x) ^ 2 + (H - y) ^ 2) - sqrt((x + dx) ^ 2 + (H - y - dy) ^ 2) - sqrt((B - x - dx) ^ 2 + (H - y - dy) ^ 2);
-  double BCD = sqrt(x ^ 2 + y ^ 2) + sqrt(x ^ 2 + (H - y) ^ 2) - sqrt((x + dx) ^ 2 + (y + dy) ^ 2) - sqrt((x + dx) ^ 2 + (H - y - dy) ^ 2);
-  double del = ACD / BCD;
+  double lACD = sqrt(pow(x, 2) + pow(H - y, 2)) + sqrt(pow(B - x, 2) + pow(H - y, 2)) - sqrt(pow(x + dx, 2) + pow(H - y - dy, 2)) - sqrt(pow(B - x - dx, 2) + pow(H - y - dy, 2));
+  double lBCD = sqrt(pow(x, 2) + pow(y, 2)) + sqrt(pow(x, 2) + pow(H - y, 2)) - sqrt(pow(x + dx, 2) + pow(y + dy, 2)) - sqrt(pow(x + dx, 2) + pow(H - y - dy, 2));
+  double del = lACD / lBCD;
   return del;
 }
 
 //Calculates the number of secondary steps that need to be taken.
 //Dir: 1 is up, 2 is down, 3 is left, 4 is right
-double calcSecondarySteps(double x, double y, int dir) {
+double calcSecondaryStep(double x, double y, int dir) {
   switch (dir) {
     case 1:
       return 1 / delta(x, y);
